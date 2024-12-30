11ba947bb5ee069c21785737a2327646910f60b0
Shreedhar <shreedhar22@gmail.com>
2024-12-29 19:25:02 -0800

    initial setup

---
+++ mosscap-wallet/app/(home)/_layout.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {View, Text} from "react-native"
+
+const HomeLayout = () => {
+  return (
+    <View>
+        <Text> Moscapp welcomes you! </Text>
+    </View>
+  )
+}
+
+export default  HomeLayout;
